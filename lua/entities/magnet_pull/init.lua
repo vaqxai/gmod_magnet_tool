b637cca35f1e28c90b3b6cdb00f81ed1bfd1e43d
Vaqxai <vaqxai@gmail.com>
2022-08-31 14:57:43 +0200

    add pull mechanic

--- lua/entities/magnet_pull/init.lua
+++ lua/entities/magnet_pull/init.lua
@@ -1,22 +1,74 @@
-ENT.Base = "base_gmodentity"
-ENT.Type = "point"
-ENT.Spawnable = false 
+AddCSLuaFile( "cl_init.lua" )
+AddCSLuaFile( "shared.lua" )
+
+include('shared.lua')
+
+function ENT:Initialize()
+  self:SetModel( "models/props_interiors/BathTub01a.mdl" )
+  self:PhysicsInit( SOLID_VPHYSICS )      -- Make us work with physics,
+  self:SetMoveType( MOVETYPE_VPHYSICS )   -- after all, gmod is a physics
+  self:SetSolid( SOLID_VPHYSICS )         -- Toolbox
+
+        local phys = self:GetPhysicsObject()
+  if (phys:IsValid()) then
+    phys:Wake()
+  end
+end
+
+hook.Add("AcceptInput", "Magnet_OnOff", function(ent, input, activator, caller, value)
+  if ent:GetClass() == "phys_magnet" then
+    for i,child_ent in ipairs(ent:GetChildren()) do
+      if child_ent:GetClass() == "magnet_pull" then
+        child_ent:MagnetInput(input, value)
+      end
+    end
+  end
+end)
+
+function ENT:MagnetInput( input, value )
+  if input == "TurnOn" then
+    self:SetEnabled(true)
+  elseif input == "TurnOff" then
+    self:SetEnabled(false)
+  end
+end
 
 function ENT:Think()
 
+  if self:GetEnabled() then
+
     local ent = self:GetParent()
 
-    local nearby_ents = ents.FindInSphere( ent:GetPos(), 500 )
-    print("Dupaa")
+    local nearby_ents = ents.FindInSphere( ent:GetPos(), (self:GetMagnetStrength() / 1000) )
     for i,nearby_ent in ipairs(nearby_ents) do
-      if nearby_ent:IsValid() and nearby_ent:GetBoneSurfaceProp(0) == "solidmetal" and (nearby_ent != ent) then
+
+      if (nearby_ent == self:GetParent()) then continue end
+      if (nearby_ent == self) then continue end
+
+      local surf = nearby_ent:GetBoneSurfaceProp(0)
+      local i = 5
+      if nearby_ent:IsValid() and string.match(surf, ".*metal.*") != nil and (nearby_ent != ent) and (nearby_ent:GetClass() != "magnet_pull") then
         local nearby_pos = nearby_ent:GetPos()
-        local this_pos = ent:GetPos()
 
-        local cross = this_pos:Cross(nearby_pos)
+        local offset = Vector(0,0,-50) 
 
-        debugoverlay.Axis(nearby_pos, cross:Angle(), 10, 1, true)
+        local this_pos = ent:LocalToWorld(offset)
+
+        local crossAng = (this_pos - nearby_pos):Angle()
+
+        local dist = this_pos:Distance(nearby_pos)
+
+        forceVec = (crossAng:Forward() * (20 * self:GetMagnetStrength())) / ( dist / 2 )
+
+        local physObj = nearby_ent:GetPhysicsObject()
+
+        if dist > 100 then
+          physObj:ApplyForceCenter(forceVec)
+        end
 
       end
     end
+
+  end
 end
+
